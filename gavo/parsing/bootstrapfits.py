bfa574355fc5bd0423657e310eff0781773fdedf
msdemlei <msdemlei@ca62dbe9-ed84-400c-aab7-804ce7e0b8cc>
2007-06-15 09:25:32 +0000

    setup.py can now probably produce a workable installation through easy_install, started Resource Descriptor generator (which is really ugly now)

---
+++ gavo/parsing/bootstrapfits.py
@@ -0,0 +1,95 @@
+"""
+A little utility to speed up processing of fits collections.
+
+It analyzes the primary header of a fits file and outputs a skeleton
+resource descriptor for it.
+
+This is *very* quick and dirty.  Let's see if something like this really
+helps and then think again.
+"""
+
+import sys
+
+from gavo import fitstools
+from gavo.parsing import resource
+
+ignoredHeaders = set(["COMMENT", "SIMPLE", "CRPIX1", "CRVAL1",
+	"CDELT1", "CTYPE1"])
+
+rdTemplate="""
+<?xml version="1.0" encoding="utf-8"?>
+
+<!DOCTYPE GavoResourceDescriptor [
+	<!ENTITY master-name "$resname$">
+]>
+
+
+<ResourceDescriptor srcdir="&master-name;/$sourcessubdir$">
+	<schema>&master-name;</schema>
+
+	<Data sourcePat="$srcpat$" id="$anything$">
+		<FitsGrammar qnd="True">
+		</FitsGrammar>
+
+		<Semantics>
+			%s
+		</Semantics>
+	</Data>
+</ResourceDescriptor>
+"""
+
+
+def makeItemXML(itemDef):
+	return '<Field dest="%s" source="%s" unit="%s" ucd="%s" description="%s">'%(
+		itemDef.get_dest(), itemDef.get_source(), itemDef.get_unit(),
+		itemDef.get_ucd(), itemDef.get_description())
+
+
+def makeRecXML(recordDef):
+	lines = ['<Record table="%s">'%recordDef.get_table()]
+	for item in recordDef.get_items():
+		lines.append("\t"+makeItemXML(item))
+	lines.append('</Record>')
+	return "\n\t\t\t".join(lines)
+
+
+def makeRdXML(recordDef):
+	"""returns the XML for a FitsGrammar resource descriptor for recordDef.
+
+	This should probably go into XML/DOM generating methods within the
+	classes themselves (or even utils.Record itself).  For now, we want it
+	quick.
+	"""
+	return rdTemplate%makeRecXML(recordDef)
+
+
+def makeRecord(cardList):
+	record = resource.RecordDef()
+	record.set_table("$tablename$")
+	for index, card in enumerate(cardList):
+		if card.key in ignoredHeaders:
+			continue
+		record.addto_items(resource.DataField(dest="$fieldname%02d$"%index, 
+			source=card.key, unit="$fillin$", ucd="$fillin$", 
+			description=card.comment))
+	return record
+
+
+def getHeaderKeys(srcName):
+	header = fitstools.openFits(srcName)[0].header
+	return header.ascardlist()
+
+
+def getUsage():
+	return "Usage: %s <fitsfile>\n"%sys.argv[0]
+
+
+def main():
+	if len(sys.argv)!=2:
+		sys.exit(getUsage())
+	recordDef = makeRecord(
+		getHeaderKeys(sys.argv[1]))
+	print makeRdXML(recordDef)
+
+if __name__=="__main__":
+	main()
--- setup.py
+++ setup.py
@@ -1,14 +1,31 @@
+import sys
+import ez_setup
+ez_setup.use_setuptools()
+
 from setuptools import setup, find_packages
+
+install_requires = ["pyfits", "pyPgSQL", "VOTable", "numarray", "elementtree"],
+if "develop" in sys.argv:
+	# No stinkin' wrapper scripts, please
+	install_requires = []
 
 setup(name="gavo",
 	description="ZAH Gavo support modules",
 	url="http://www.g-vo.org",
+	license="GPL",
 	author="Markus Demleitner",
 	author_email="msdemlei@ari.uni-heidelberg.de",
-	packages=["gavo", "gavo/parsing", "gavo/web", "gavo/web/querulator"],
+	packages=find_packages(),
+	py_modules=["ez_setup"],
+	install_requires=install_requires,
+	dependency_links=["http://ara.ari.uni-heidelberg.de/soft/python",
+		"http://sourceforge.net/project/showfiles.php?group_id=16528",
+		"http://sourceforge.net/project/showfiles.php?group_id=1369",
+		"http://www.stsci.edu/resources/software_hardware/pyfits/Download"],
 	entry_points={
 		'console_scripts': [
 			'gavoimp = gavo.parsing.commandline:main',
+			'gavomkrd = gavo.parsing.bootstrapfits:main',
 		]
 	},
 	version="0.2")
