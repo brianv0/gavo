e893211faa385c48eb0477d822ca706d842cbe07
msdemlei <msdemlei@ca62dbe9-ed84-400c-aab7-804ce7e0b8cc>
2007-07-12 07:30:41 +0000

    The forgotten files for the beginning of masquerator.

---
+++ gavo/nullui.py
@@ -0,0 +1,35 @@
+"""
+This is a ui that swallows all output.  You'll want this when you're
+talking to the web.
+"""
+
+import gavo
+
+
+class NullCounter:
+	def __init__(self, *args):
+		pass
+	
+	def hit(*args):
+		pass
+	
+	def unhit(*args):
+		pass
+	
+	def hitBad(*args):
+		pass
+	
+	def close(*args):
+		pass
+
+
+class NullUi:
+	def displayMessage(*args):
+		pass
+	displayError = displayMessage
+	
+	def getGoodBadCounter(self, *args):
+		return NullCounter()
+
+
+gavo.ui = NullUi()
