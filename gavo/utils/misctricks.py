0ce454553e9afe0711ce50ecb870493fde7c03a3
msdemlei <msdemlei@ca62dbe9-ed84-400c-aab7-804ce7e0b8cc>
2016-12-01 15:29:24 +0000

    Adding a samplerd interpreted text role for links into the GAVO RD repo.

--- gavo/utils/misctricks.py
+++ gavo/utils/misctricks.py
@@ -164,6 +164,18 @@ try:
 	RSTExtensions.makeTextRole("dachsref", _dachsrefRoleFunc)
 	del _dachsrefRoleFunc
 
+	def _samplerdRoleFunc(name, rawText, text, lineno, inliner, 
+			options={}, content=[]):
+		# this will turn into a link to a file in the GAVO svn
+		# (usually for RDs)
+		text = rstutils.unescape(text)
+		url = "http://svn.ari.uni-heidelberg.de/svn/gavo/hdinputs/"+text
+		return [nodes.reference(text, text, internal=False, refuri=url)
+			], []
+
+	RSTExtensions.makeTextRole("samplerd", _samplerdRoleFunc)
+	del _samplerdRoleFunc
+
 except ImportError:
 	rstcore = NotInstalledModuleStub("docutils") #noflake: conditional import
 
